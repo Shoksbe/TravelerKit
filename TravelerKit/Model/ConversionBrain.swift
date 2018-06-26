879e2fd324fec407fd456d18570119db63b9a027
Shoksbe <deknyfgregory@gmail.com>
2018-06-26 22:37:28 +0200

    feat(ConversionBRain): [func getCurrencyRates], treatment of the failure part

--- TravelerKit/Model/ConversionBrain.swift
+++ TravelerKit/Model/ConversionBrain.swift
@@ -13,9 +13,34 @@ class ConversionBrain {
      it is equal to the amount displayed in the textField of the original currency.*/
     var unConvertedAmount: String = ""
 
+    private var error: String?
 
     /// Currencies and their exchange rate
     private var currencyRates: [String: Double]?
+
+    /*
+    Manual storage of all possible error codes because the API
+     does not provide a description of the error each time.
+    */
+    private let errorCodeDescription =
+        [404:"The requested resource does not exist.",
+         101:"No API Key was specified or an invalid API Key was specified.",
+         103:"The requested API endpoint does not exist.",
+         104:"The maximum allowed API amount of monthly API requests has been reached.",
+         105:"The current subscription plan does not support this API endpoint.",
+         106:"The current request did not return any results.",
+         102:"The account this API request is coming from is inactive.",
+         201:"An invalid base currency has been entered.",
+         202:"One or more invalid symbols have been specified.",
+         301:"No date has been specified.",
+         302:"An invalid date has been specified.",
+         403:"No or an invalid amount has been specified.",
+         501:"No or an invalid timeframe has been specified.",
+         502:"No or an invalid \"start_date\" has been specified.",
+         503:"No or an invalid \"end_date\" has been specified.",
+         504:"An invalid timeframe has been specified.",
+         505:"The specified timeframe is too long, exceeding 365 days."
+    ]
 
     /// Add a number to the unConvertedAmount string
     ///
@@ -43,6 +68,21 @@ class ConversionBrain {
             //If the service call has worked and the API has returned the rates
             if success, let rates = request?.rates {
                 self.currencyRates = rates
+            } else {
+
+                //Check if there is an error in the API request
+                guard let error = request?.error else {
+                    self.error = "Error from service"
+                    return
+                }
+
+                //Check if there is a description of the error with the error code of the API
+                guard let errorDescription = self.errorCodeDescription [error.code] else {
+                    self.error = "Unknow error"
+                    return
+                }
+
+                self.error = errorDescription
             }
         }
     }
