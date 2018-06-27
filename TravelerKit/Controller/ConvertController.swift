1ad34cfcb967078a5ae86d7180e4054a6ba9f28d
Shoksbe <deknyfgregory@gmail.com>
2018-06-27 09:06:25 +0200

    feat(ConverterController): creation of convertController and connection with the view.

---
+++ TravelerKit/Controller/ConvertController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  TravelerKit
+//
+//  Created by De knyf Gregory on 23/06/18.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import UIKit
+
+class ConvertController: UIViewController {
+
+}
