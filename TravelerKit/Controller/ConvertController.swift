2316bb14cecdb33e16bb74f83efd05b9a967a618
Shoksbe <deknyfgregory@gmail.com>
2018-06-27 09:16:37 +0200

    feat(ConverterController): connection between the keyboard and unConvertedAmountTextField
    When the user does an action on the keyboard, the textField responds.

--- TravelerKit/Controller/ConvertController.swift
+++ TravelerKit/Controller/ConvertController.swift
@@ -9,5 +9,42 @@
 import UIKit
 
 class ConvertController: UIViewController {
+    // MARK: - Properties
+    private var conversionBrain: ConversionBrain!
 
+    // MARK: - Outlets
+    @IBOutlet weak var unConvertedAmountTextField: UITextField!
+}
+
+// MARK: - Methods
+extension ConvertController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        conversionBrain = ConversionBrain()
+    }
+
+    ///Update the content of the unConvertedamount textfield
+    private func updateDisplays() {
+        //Unconverted display
+        unConvertedAmountTextField.text = conversionBrain.unConvertedAmount
+    }
+}
+
+// MARK: - Actions
+extension ConvertController {
+    @IBAction func didPressedKeyboardButton(_ sender: UIButton) {
+        guard let buttonValue = sender.titleLabel?.text else { return }
+
+        switch buttonValue {
+        case "0","1","2","3","4","5","6","7","8","9":
+            conversionBrain.addNumber(buttonValue)
+        case ".":
+            conversionBrain.addComma()
+        case "del":
+            conversionBrain.resetToZero()
+        default:
+            return
+        }
+        updateDisplays()
+    }
 }
