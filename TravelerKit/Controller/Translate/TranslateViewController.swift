50cc6cb806652478ed72a0572bea5019c8030038
Shoksbe <deknyfgregory@gmail.com>
2018-07-20 10:35:50 +0200

    feat(TranslateViewController): make a connection between TranslateView and his controller with outlets

---
+++ TravelerKit/Controller/Translate/TranslateViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateViewController.swift
+//  TravelerKit
+//
+//  Created by Gregory De knyf on 20/07/18.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import UIKit
+
+class TranslateViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var toBeTranslatedTextView: UITextView!
+    @IBOutlet weak var translatedTextView: UITextView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+}
