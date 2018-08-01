850b6950562768ca5ea03609efab40e650c87564
Shoksbe <deknyfgregory@gmail.com>
2018-08-01 08:56:54 +0200

    feat(TranslateDecodable): add a decodable file using to get info from api

---
+++ TravelerKit/Model/Translate/TranslationDecodable.swift
@@ -0,0 +1,21 @@
+//
+//  TranslationDecodable.swift
+//  TravelerKit
+//
+//  Created by Gregory De knyf on 1/08/18.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import Foundation
+
+//Structure use for parse data from api
+struct Translation: Decodable {
+    struct Data: Decodable {
+        struct Translation: Decodable {
+            let translatedText: String
+            let detectedSourceLanguage: String
+        }
+        let translations: [Translation]
+    }
+    let data: Data
+}
