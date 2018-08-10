bbb1fb7cc77d61050f9b4a935a18d7928a4eff81
Shoksbe <deknyfgregory@gmail.com>
2018-08-10 19:40:51 +0200

    feat(WeatherDecodable): add a decodable file using to get info from api

---
+++ TravelerKit/Model/Weather/WeatherDecodable.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherDecodable.swift
+//  TravelerKit
+//
+//  Created by De knyf Gregory on 10/08/18.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherDetails: Decodable {
+    struct Query: Decodable {
+        struct Result: Decodable {
+            struct Channel: Decodable {
+                struct Location: Decodable {
+                    let city: String
+                }
+                let location : Location
+                struct Item: Decodable {
+                    struct Condition: Decodable {
+                        let code: String
+                        let temp: String
+                        let text: String
+                    }
+                    let condition: Condition
+                    struct Forecast: Decodable {
+                        let code: String
+                        let date: String
+                        let day: String
+                        let high: String
+                        let low: String
+                        let text: String
+                    }
+                    let forecast: [Forecast]
+                }
+                let item: Item
+            }
+            let channel: Channel
+        }
+        let created: String
+        let results: Result
+    }
+    let query: Query
+}
