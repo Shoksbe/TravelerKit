d286e5d032e88372c67764a75379666303d95ab5
Shoksbe <deknyfgregory@gmail.com>
2018-08-11 08:58:09 +0200

    feat(Weather): Grouping information to allow easier reading of json data

---
+++ TravelerKit/Model/Weather/WeatherInfo.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherInfo.swift
+//  TravelerKit
+//
+//  Created by De knyf Gregory on 11/08/18.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct WeatherInfo {
+    let city: String
+    let code: String
+    let temp: String
+    let text: String
+    let icon: UIImage
+    let high: String
+    let low: String
+}
