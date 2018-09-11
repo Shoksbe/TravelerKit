f094533211491974d335c170bd864ea697f95dff
Shoksbe <deknyfgregory@gmail.com>
2018-09-11 16:21:01 +0200

    refact(TranslateService): using a urlRequest instead of url

--- TravelerKit/Model/Translate/TranslateService.swift
+++ TravelerKit/Model/Translate/TranslateService.swift
@@ -14,46 +14,48 @@ class TranslateService {
     private init() {}
     
     private var task: URLSessionDataTask?
-    
     private var session = URLSession(configuration: .default)
-    
     init(session: URLSession) {
         self.session = session
     }
+
+    private let translateUrl = URL(string: "https://translation.googleapis.com/language/translate/v2")!
     
     let apiKey = valueForAPIKey(named: "API_CLIENT_GOOGLE")
     
-    func getTranslation(of text: String,to language: String, callback: @escaping (Bool, TranslationDecodable?)-> ()) {
-        
-        var urlString = "https://translation.googleapis.com/language/translate/v2?q=\(text)&target=\(language)&key=\(apiKey)"
-        urlString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-        
-        guard let url = URL(string: urlString) else {
-            callback(false, nil)
-            return
-        }
-        
+    func getTranslation(of text: String,to language: String, callback: @escaping (Bool, String?)-> ()) {
+
+        //Create request
+        var request = URLRequest(url: translateUrl)
+        request.httpMethod = "POST"
+
+        //add the body of the request
+        let body = "q=\(text)&target=\(language)&key=\(apiKey)"
+        request.httpBody = body.data(using: .utf8)
+
+        //Stop the current task if there is one so that you do not run multiple tasks at the same time
         task?.cancel()
-        task = session.dataTask(with: url) { (data, response, error) in
+
+        //Create new task
+        task = session.dataTask(with: request) { (data, response, error) in
             DispatchQueue.main.async {
-                
+
                 guard let data = data, error == nil,
                     let response = response as? HTTPURLResponse, response.statusCode == 200 else {
                         callback(false, nil)
                         return
                 }
-                
-                do {
-                    let decoder = JSONDecoder()
-                    let obj = try decoder.decode(TranslationDecodable.self, from: data)
-                    callback(true, obj)
+
+                guard let responseJSON = try? JSONDecoder().decode(TranslationDecodable.self, from: data),
+                let translation = responseJSON.data.translations.first?.translatedText else {
+                        callback(false, nil)
+                        return
                 }
-                catch let jsonErr {
-                    print("Failed to decode:", jsonErr)
-                    callback(false, nil)
-                }
+
+                callback(true, translation)
             }
         }
+        //Launch task
         task?.resume()
     }
 }
